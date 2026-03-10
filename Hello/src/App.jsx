e6b9921337853fdf4fa89faae8a77930e6656be7
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2026-03-10 19:15:44 +0000

    How to Babel compiler

--- Hello/src/App.jsx
+++ Hello/src/App.jsx
@@ -1,3 +1,14 @@
 export const App = () =>{
   return <h1>Hello Rohit Mourya</h1>;
+
+  
 };
+
+
+
+
+// Task 2
+// Babel : It is JS compiler (React -> JS code)
+return React.createElement("h1", null, "Hello Rohit Mourya");
+
+
