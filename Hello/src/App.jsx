d0f53c2e4064fde0d3153e10275af64aeca942fc
rohitmouryar <125853279+rohitmouryar@users.noreply.github.com>
2026-03-11 09:28:52 +0530

    Use first Reaact component

--- Hello/src/App.jsx
+++ Hello/src/App.jsx
@@ -1,14 +1,27 @@
-export const App = () =>{
-  return <h1>Hello Rohit Mourya</h1>;
-
-  
-};
-
-
+// export const App = () =>{
+//   return <h1>Hello Rohit Mourya</h1>;  
+// };
 
 
 // Task 2
 // Babel : It is JS compiler (React -> JS code)
-return React.createElement("h1", null, "Hello Rohit Mourya");
+// return React.createElement("h1", null, "Hello Rohit Mourya");
 
 
+// Tutorial 7: ComponentS
+// Step 1: Create a component
+ const Greeting = () =>{
+  return <h1>Hello Rohit Mourya</h1>;  
+}
+
+// Step 2: Use the component
+export const App = () =>{
+  return(
+    <div>
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
+    </div>
+  )
+}
