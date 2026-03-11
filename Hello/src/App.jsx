3e9ec77627c62861c75c85360d625a53440c3e43
rohitmouryar <125853279+rohitmouryar@users.noreply.github.com>
2026-03-11 10:14:53 +0530

    React Fragment : To remove extra node in their code

--- Hello/src/App.jsx
+++ Hello/src/App.jsx
@@ -8,14 +8,16 @@
 // return React.createElement("h1", null, "Hello Rohit Mourya");
 
 
-// Tutorial 7: ComponentS
+// Tutorial 6: ComponentS
+
 // Step 1: Create a component
  const Greeting = () =>{
   return <h1>Hello Rohit Mourya</h1>;  
 }
 
+
 // Step 2: Use the component
-export const App = () =>{
+/*export const App = () =>{
   return(
     <div>
       <Greeting />
@@ -23,5 +25,50 @@ export const App = () =>{
       <Greeting />
       <Greeting />
     </div>
+  )
+}
+*/
+
+
+
+// Tutorial 7: React Fragments
+
+// How to remove multiple node from the DOM 
+// Step 1 Using Array
+/*export const App = () =>{
+  return([
+    <Greeting key={1} />,
+    <Greeting key={2} />,
+    <Greeting key={3} />,
+    <Greeting key={4} />
+  ]
+    
+  )
+}
+  */
+
+// Step 2: Using React Fragment
+// import React, { Fragment } from "react";  
+import { Fragment } from "react";
+
+export const App = () =>{
+  return(
+    // <React.Fragment>
+    //   <Greeting />
+    //   <Greeting />
+    //   <Greeting />
+    //   <Greeting />
+    // </React.Fragment>
+
+    // <Fragment>
+    //   <Greeting />
+    // </Fragment>
+
+    <>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    </>
   )
 }
